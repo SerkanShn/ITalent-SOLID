eab164872b294ea8decb74de6008fb25acbf581f
serkan.sahin <serkansahin114@gmail.com>
2022-08-14 15:14:47 +0300

    LogError classı eklendi

---
+++ ITalent-SOLİD/SRP/LogError.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITalent_SOLİD.SRP
+{
+    class LogError
+    {
+        public void LogErrorToTextFile(string error)
+        {
+            Console.WriteLine("Dosyaya log kaydı eklendi");
+        }
+
+        public void LogErrorToOracleDatabase(string error)
+        {
+            Console.WriteLine("Oracle veritabanına log kaydı eklendi");
+        }
+
+        public void LogErrorToSQLDatabase(string error)
+        {
+            Console.WriteLine("SQL veritabanına log kaydı eklendi");
+        }
+
+        public string GetErrorLogs(string type)
+        {
+            return type switch
+            {
+                "FromSQLDatabase" => "SQL veritabanından loglar alındı",
+                "FromOracleDatabase" => "Oracle veritabanından loglar alındı",
+                "FromTextFile" => "Dosyadan loglar alındı",
+
+            };
+        }
+
+        public int GetErrorLogsCount(string type)
+        {
+            return type switch
+            {
+                "FromSQLDatabase" => 10,
+                "FromOracleDatabase" => 20,
+                "FromTextFile" => 30,
+
+            };
+        }
+    }
+}
